9f7ee9a4046e6be827059bd69dd91bc7447bb172
Virag Kormoczy <vkormoczy@gmail.com>
2023-01-28 16:33:34 +0200

    Add favorite blog tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,21 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return [];
+  }
+
+  const likesArray = [];
+  blogs.map((blog) => likesArray.push(blog.likes));
+  const maximumLikes = Math.max(...likesArray);
+  const index = likesArray.indexOf(maximumLikes);
+
+  return blogs[index];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
